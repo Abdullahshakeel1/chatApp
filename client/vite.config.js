d175cb84799d281c66d0b27f99c69bfb62721e30
Abdullahshakeel1 <abdullahsh74189@gmail.com>
2024-10-16 21:43:45 +0500

    dvdsv

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,33 +1,33 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+
+export default defineConfig({
+  plugins: [react()],
+  server:{
+ 
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure:false, 
+      },
+    },
+  }
+})
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   server:{
- 
+//   server: {
 //     proxy: {
 //       '/api': {
-//         target: 'http://localhost:3000',
-//         secure:false, 
+//         target: 'https://chat-app-vp7f.vercel.app', // Your backend URL
+//         changeOrigin: true, // Changes the origin of the host header to the target URL
+//         secure: false, // Allow self-signed certs (if applicable)
 //       },
 //     },
-//   }
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://chat-app-omega-ebon.vercel.app', // Your backend URL
-        changeOrigin: true, // Changes the origin of the host header to the target URL
-        secure: false, // Allow self-signed certs (if applicable)
-      },
-    },
-  },
-});
+//   },
+// });
