4f32c5b2956922274a547eb3430584267749fe7e
Abdullahshakeel1 <abdullahsh74189@gmail.com>
2024-10-16 17:09:07 +0500

    testing

--- client/src/context/socketContext.jsx
+++ client/src/context/socketContext.jsx
@@ -13,7 +13,7 @@ export const SocketContextProvider =({children})=>{
     useEffect(() => {
         if (authUser) {
             const socket = io('https://chat-app-omega-ebon.vercel.app', {
-                query: { userId: authUser._id }
+                auth: { userId: authUser._id }
             });
             setSocket(socket);
     
--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 // Setup Socket.IO with CORS
  const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: '*',
         methods: ['GET', 'POST'],
         credentials: true
     }
